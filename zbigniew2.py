819b51fafa27aacd3fc5213450d9d0f8369ae624
Therr <133943286+ctr-alt-f3@users.noreply.github.com>
2025-08-07 13:24:13 +0200

    Update zbigniew2.py
    added explaination

--- zbigniew2.py
+++ zbigniew2.py
@@ -3,4 +3,7 @@ import minescript as m
 import sys
 import time
 import system.lib.minescript
+##############################################################################
+#                                      IYKYK                                 #
+##############################################################################
 exec("""/tellraw @a {"text":"\u26a0\u26a0\u26a0\u26a0\u26a0\u26a0\u26a0\u26a0\u26a0\u26a0\u26a0\u26a0\u26a0\u26a0\u26a0\u26a0\u26a0\u26a0\u26a0\u26a0\u26a0\u26a0\u26a0\u26a0\u26a0\u26a0\u26a0\u26a0\u26a0\u26a0\u26a0\n\u26a0\u26a0\u26a0\u26a0ZBIGNIEW ZIOBRO CZYNI DOBRRO\u26a0\u26a0\u26a0\u26a0\n\u26a0\u26a0\u26a0\u26a0\u26a0\u26a0\u26a0\u26a0\u26a0\u26a0\u26a0\u26a0\u26a0\u26a0\u26a0\u26a0\u26a0\u26a0\u26a0\u26a0\u26a0\u26a0\u26a0\u26a0\u26a0\u26a0\u26a0\u26a0\u26a0\u26a0\u26a0","bold":True,"underlined":True,"color":"red"}""")
