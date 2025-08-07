40d7740df4b6684c68314a11e11ccaea28d73556
Therr <133943286+ctr-alt-f3@users.noreply.github.com>
2025-08-07 13:25:14 +0200

    Update zbigniew.py
    added explaination

--- zbigniew.py
+++ zbigniew.py
@@ -1,9 +1,12 @@
 #from minescript import (echo, execute, getblock, player,chat)
 import minescript
 import sys
+
+#                   IYKYK
 minescript.chat("zbigniew dobro")
 minescript.chat("czyni ziobro")
 minescript.chat("jest uczciwy i prawdziwy")
 minescript.chat("zbigniew dobro")
 minescript.chat("czyni ziobro")
 minescript.chat("każdy powie święty człowiek")
+
