06688abe8e1e583d0c24410f0a0e53c631f16985
Therr <133943286+ctr-alt-f3@users.noreply.github.com>
2025-08-09 17:07:35 +0200

    Delete summonTNT.py

--- summonTNT.py
+++
@@ -1,9 +0,0 @@
-import pyautogui as pag
-import time
-import minescript as m
-import sys
-from minescript_plus import Client,Gui,Keybind
-
-while True:
-    m.execute("summon minecraft:tnt ~ ~1 ~")
-    time.sleep(1)
